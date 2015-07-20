29a45b5434604e536ebada7c78e7d492ff90fdc5
Maxim Xu <xujialv@gmail.com>
2015-07-21 01:21:17 +0800

    a

--- js/script.js
+++ js/script.js
@@ -63,7 +63,7 @@ var n = 1,
         $("#choiceA").text(v.A.describe);
         $("#choiceB").text(v.B.describe);
         $("#choiceC").text(v.C.describe);
-        audio.oncanplaythrough = document.getElementById("song").play();
+        document.getElementById("song").oncanplaythrough = document.getElementById("song").play();
         playing = true;
         n++;
      if (n > 10) {   
